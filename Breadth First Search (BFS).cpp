1f11c86a4358bd96e9f9c4fe24b96abe49888bce
Harsh PIpal <73478379+Harsh-pipal@users.noreply.github.com>
2021-09-03 18:41:59 +0530

    Update Breadth First Search (BFS).cpp

--- Breadth First Search (BFS).cpp
+++ Breadth First Search (BFS).cpp
@@ -8,47 +8,65 @@
 #define vi vector<int>
 #define vii vector<ii>
 #define ll long long 
-#define INF 1000000000
+#define INF 1e18
 #define nl '\n'
 const double PI = 3.141592653589793238460;
 typedef std::complex<double> Complex;
 typedef std::valarray<Complex> CArray;
 using namespace std;
 
-	
-void solve(){
-	int nodes,edges;
-	cin>>nodes>>edges;
-	bool visited[nodes+1];
-	vector<int>adj[nodes+1];
-	for(int i=0;i<=nodes;i++){
-		visited[i]=false;
-	}
-	int a,b;
-	for(int i=0;i<edges;i++){
-		cin>>a>>b;
-		adj[a].pb(b);
-		adj[b].pb(a);
-	}
-	queue<int>q;
-	q.push(1);
-	visited[1]=true;
-	while(!q.empty()){
-		int  node=q.front();
-		
-		q.pop();
-		cout<<node<<" ";
-		for(int i=0;i<adj[node].size();i++){
-			if(visited[adj[node][i]]==false){
-				visited[adj[node][i]]=true;
-				q.push(adj[node][i]);
-			}
-		}
-	}
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
 }
+
+void solve(){ 
+    int n,m;
+    cin>>n>>m;
+    vector<int>adj[n+1];
+    vector<int>visited(n+1,0);
+    for(int i=0;i<m;i++){
+        int a,b;
+        cin>>a>>b;
+        adj[a].pb(b);
+        adj[b].pb(a);
+    }
+    vector<int>bfsOrder;
+    for(int i=1;i<=n;i++){
+        if (visited[i]==0){
+            queue<int>q;
+            q.push(i);
+            visited[i]=1;
+            while(!q.empty()){
+                int node=q.front();
+                q.pop();
+                bfsOrder.pb(node);
+                for(auto it:adj[node]){
+                    if(visited[it]==0){
+                        q.push(it); 
+                        visited[it]=1;                        
+                    }                    
+                }
+            }
+        }
+    }
+    rep(i,n)cout<<bfsOrder[i]<<" ";
+    cout<<nl;
+
+}   
+ 
 int main(){
-	int T=1;
-	//cin>>T;
-	while(T--)
-	solve();
-}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);    
+    cout<<fixed<<setprecision(10);
+    int T=1;
+    //cin>>T;
+    while(T--)
+    solve();
+}       
