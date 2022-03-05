945ffcaf6e41a427a2a54d47dc6290953d8a92c7
Harsh PIpal <73478379+Harsh-pipal@users.noreply.github.com>
2022-03-05 13:44:45 +0530

    Create Cycle detection in Undirected graph using BFS.cpp

---
+++ Cycle detection in Undirected graph using BFS.cpp
@@ -0,0 +1,126 @@
+//Author @Harsh_xerus
+
+#include<bits/stdc++.h>
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#define rep(i,n) for (int i = 0; i < n; i++)
+#define mem0(arr) memset(arr, 0, sizeof(arr))
+#define mod 1000000007
+#define mod2 998244353
+#define pb push_back
+#define ff first
+#define ss second
+#define ll long long 
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define INF 1e18
+#define nl '\n'
+#define vi vector<int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+const ll maxn = 1e6 + 2;
+const double PI = 3.141592653589793238460;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+typedef unsigned long long ull;
+typedef long double lld;
+using namespace std;
+
+
+//--------------------------Helper_functions-------------------------------//
+
+//-------------------------------GCD---------------------------------------//
+
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+
+//-------------------------------LCM---------------------------------------//
+
+ll lcm(ll a, ll b){return (a * b) / gcd(a, b);}
+
+//----------------------------Factorial------------------------------------//
+
+ll fact(ll x){if(x==0)return 1;return (x*fact(x-1))%mod;}
+
+//-----------------------------Bin_Expo------------------------------------//
+
+ll binpow(ll a,ll b) {
+     ll res = 1;
+     while (b > 0) {
+         if (b & 1)
+             res = (res * a)%mod;
+         a = (a * a)%mod;
+         b >>= 1;
+     }
+     return res%mod;
+}
+
+//-------------------------Sum/Count of digits----------------------------//
+
+ll sd(ll x){ll temp=0;while(x>0){temp+=x%10;x/=10;}return temp;}
+ll cd(ll x){ll temp=0;while(x>0){temp++;x/=10;}return temp;}
+
+//-------------------------------START-------------------------------------//
+
+//cycle detection in undirected graph using BFS
+
+int n,m;
+vector<int>adj[1000];
+vector<int>vis(1001,0);
+
+bool iscycle(int node){
+    vis[node]=1;
+    queue<pair<int,int>>q;
+    q.push({node,-1});
+    while(!q.empty()){
+        int f=q.front().first;
+        int par=q.front().second;
+        q.pop();
+        for(auto x:adj[f]){
+            //cout<<x<<" ";   
+            if(!vis[x]){
+                vis[x]=1;
+                q.push({x,f});
+            }
+            else if(par!=x){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+void solve(){ 
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    ll noc=0;
+    bool cycle = false;
+    for(int i=1;i<=n;i++){
+        if(!vis[i]){
+            if(iscycle(i))
+                cycle=true;
+        }
+    }
+    cout<<( cycle ? "Present" : "not present" )<<nl;   
+}
+
+int main(){
+     
+    ll t = 1, i = 1;
+    //cin >> t;
+    cout << std::setprecision(15);
+    //presolve();
+    
+    while (t--)
+    {
+        //cout << "Case #" << i << ": ";   //only during google contest
+        //i++;                             //only during google contest
+    
+        solve();
+    }
+}
