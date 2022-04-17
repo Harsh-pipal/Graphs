6230bc951f1ce41ce39d0e1c3213204d68670b23
Harsh PIpal <73478379+Harsh-pipal@users.noreply.github.com>
2022-04-17 11:29:14 +0530

    Create Kruskal's Algorithm (MST).cpp

---
+++ Kruskal's Algorithm (MST).cpp
@@ -0,0 +1,169 @@
+//Author @Harsh_xerus
+
+#include<bits/stdc++.h>
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#define rep(i,n) for (int i = 0; i < n; i++)
+#define mem0(arr) memset(arr, 0, sizeof(arr))
+#define mod 1000000007
+#define mod2 998244353
+#define pb push_back
+#define ff first
+#define ss second
+#define ll long long 
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define vpll vector<pll>
+#define INF 1e18
+#define nl '\n'
+#define vi vector<int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+const ll maxn = 1e6 + 2;
+const double PI = 3.141592653589793238460;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+typedef unsigned long long ull;
+typedef long double lld;
+using namespace std;
+
+
+//--------------------------Helper_functions-------------------------------//
+
+//-------------------------------GCD---------------------------------------//
+
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+
+//-------------------------------LCM---------------------------------------//
+
+ll lcm(ll a, ll b){return (a * b) / gcd(a, b);}
+
+//----------------------------Factorial------------------------------------//
+
+ll fact(ll x){if(x==0)return 1;return (x*fact(x-1))%mod;}
+
+//-----------------------------Bin_Expo------------------------------------//
+
+ll binpow(ll a,ll b) {
+     ll res = 1;
+     while (b > 0) {
+         if (b & 1)
+             res = (res * a)%mod;
+         a = (a * a)%mod;
+         b >>= 1;
+     }
+     return res%mod;
+}
+
+//-------------------------Sum/Count of digits----------------------------//
+
+ll sd(ll x){ll temp=0;while(x>0){temp+=x%10;x/=10;}return temp;}
+ll cd(ll x){ll temp=0;while(x>0){temp++;x/=10;}return temp;}
+
+//-------------------------------START-------------------------------------//
+
+//Kruskal algorithm ....finding the minimun spanning tree...
+//a tree with n nodes and n-1 edges and the weight of this tree should be 
+//minimum.
+
+int n, m;
+vector<int>vis(maxn,0);
+vector<pair<int,int>>adj[maxn];
+ll par[10000];
+ll ran[10000];
+
+//initializing parents to itself and rank to zero
+
+struct node {
+    int u;
+    int v;
+    int wt; 
+    node(int first, int second, int weight) {
+        u = first;
+        v = second;
+        wt = weight;
+    }
+};
+
+bool comp(node a, node b){
+    return a.wt<b.wt;
+}
+
+//we are using path compression to reduce the time complexity to O(4x) from logn
+
+void makeset(){
+    for(int i=1;i<=n;i++){
+        par[i]=i;
+        ran[i]=0;
+    }
+}
+
+int findpar(int u){
+    if(u==par[u])
+        return u;
+
+    return par[u]=findpar(par[u]);
+}
+
+//attaching the node with less rank value to higher one else increase rank by 1
+
+void unio(int u , int v){
+    u=findpar(u);
+    v=findpar(v);
+
+    if(ran[u]<ran[v]){
+        par[u]=v;
+    }
+    else if(ran[v]<ran[u]){
+        par[v]=u;
+    }
+    else{
+        par[v]=u;
+        ran[u]++;
+    }
+}
+
+
+
+void solve(){ 
+    cin>>n>>m;
+    makeset();
+    vector<node> edges; 
+    for(int i = 0;i<m;i++) {
+        int u, v, wt;
+        cin >> u >> v >> wt; 
+        edges.push_back(node(u, v, wt)); 
+    }
+    sort(edges.begin(), edges.end(), comp); 
+
+    int MSTcost = 0;
+    vector<pair<int,int>>mst;
+    for(auto it:edges){
+        if(findpar(it.u)!=findpar(it.v)){
+            MSTcost+=it.wt;
+            mst.pb({it.u,it.v});
+            unio(it.u,it.v);
+        }
+    }
+    cout<<MSTcost<<nl;
+    for(auto it:mst){
+        cout<<it.ff<<" "<<it.ss<<nl;
+    }
+
+}
+
+int main(){
+    
+    ll t = 1, i = 1;
+    //cin >> t;
+    cout << std::setprecision(15);
+    //presolve();
+    
+    while (t--)
+    {
+        //cout << "Case #" << i << ": ";   //only during google contest
+        //i++;                             //only during google contest
+    
+        solve();
+    }
+}
